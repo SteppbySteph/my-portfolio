c3c1975684fc2af0114b8c7470daf0f03445a0f0
StepbyStepp <91485842+SteppbySteph@users.noreply.github.com>
2023-05-30 12:42:52 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./pages/Header";
+import About from "./pages/About";
+import TechStack from "./pages/TechStack";
+import FeaturedProjects from "./pages/FeaturedProjects";
+//import OtherProjects from ".pages/OtherProjects";
+import Thoughts from "./pages/Thoughts";
+import Skills from "./pages/Skills";
+import More from "./pages/More";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <About />
+      <TechStack />
+      <FeaturedProjects />
+      <Thoughts />
+      <Skills />
+      <More />
+      <Contact />
     </div>
   );
 }
